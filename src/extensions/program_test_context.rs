683634e961a18427333e1c4b3f33e75a978855eb
f44z <vgood@protonmail.com>
2022-05-13 16:21:26 +0200

    Calculate slot and manually set timestamp

--- src/extensions/program_test_context.rs
+++ src/extensions/program_test_context.rs
@@ -1,28 +1,42 @@
 use async_trait::async_trait;
-use solana_program_test::ProgramTestContext;
+use solana_program_test::{ProgramTestContext, ProgramTestError};
 use solana_sdk::sysvar::clock::Clock;
 
 #[async_trait]
 pub trait ProgramTestContextExtension {
     /// Calculate slot number from the provided timestamp
-    async fn warp_to_timestamp(&mut self, timestamp: i64);
+    async fn warp_to_timestamp(&mut self, timestamp: i64) -> Result<(), ProgramTestError>;
 }
 
 #[async_trait]
 impl ProgramTestContextExtension for ProgramTestContext {
-    async fn warp_to_timestamp(&mut self, timestamp: i64) {
-        let clock: Clock = self.banks_client.get_sysvar().await.unwrap();
-        let mut slot = clock.slot;
-        let mut now = clock.unix_timestamp;
+    async fn warp_to_timestamp(&mut self, timestamp: i64) -> Result<(), ProgramTestError> {
+        let mut clock: Clock = self.banks_client.get_sysvar().await.unwrap();
+        let now = clock.unix_timestamp;
+        let current_slot = clock.slot;
+        clock.unix_timestamp = timestamp;
 
-        while now < timestamp {
-            slot = slot + 2;
-            self.warp_to_slot(slot);
-            let clock: Clock = self.banks_client.get_sysvar().await.unwrap();
-            now = clock.unix_timestamp;
+        if now >= timestamp {
+            println!("Timestamp incorrect. Cannot turn time back.");
+            return Err(ProgramTestError::InvalidWarpSlot);
         }
 
-        let clock: Clock = self.banks_client.get_sysvar().await.unwrap();
-        now = clock.unix_timestamp;
+        let ns_per_slot = self.genesis_config().ns_per_slot();
+        let timestamp_diff_ns = timestamp
+            .checked_sub(now) //calculate time diff
+            .expect("Problem with sub new timestamp with current one")
+            .checked_mul(1000000000) //convert from s to ns
+            .expect("Problem with sub new timestamp with current one")
+            as u128;
+
+        let slots = timestamp_diff_ns
+            .checked_div(ns_per_slot)
+            .expect("Problem with sub new timestamp with current one") as u64;
+
+        self.set_sysvar(&clock);
+        self.warp_to_slot(current_slot + slots)
+            .expect("Cannot move to timestamp");
+
+        Ok(())
     }
 }
