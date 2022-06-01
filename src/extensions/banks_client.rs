9ae238daf42c74173b9cdebde3308105ef777817
test <test@test.com>
2022-06-01 11:58:19 +0000

    Fix formatting

--- src/extensions/banks_client.rs
+++ src/extensions/banks_client.rs
@@ -3,25 +3,36 @@ use solana_program::program_pack::Pack;
 use solana_sdk::{
     instruction::Instruction,
     pubkey::Pubkey,
-    signature::{Keypair, Signer},
+    signature::{
+        Keypair,
+        Signer
+    },
     system_transaction,
     sysvar::rent::Rent,
     transaction::Transaction,
-    transport,
+    transport
 };
 
-use spl_associated_token_account::{create_associated_token_account, get_associated_token_address};
+use spl_associated_token_account::{
+    get_associated_token_address,
+    create_associated_token_account
+};
 
 #[cfg(feature = "anchor")]
 use anchor_lang::AccountDeserialize;
 
 #[cfg(feature = "anchor")]
-use futures::{Future, FutureExt};
+use futures::{
+    Future,
+    FutureExt
+};
 
 #[cfg(feature = "anchor")]
 use std::pin::Pin;
 
-pub use solana_banks_client::{BanksClient, BanksClientError};
+pub use {
+    solana_banks_client::{BanksClient, BanksClientError},
+};
 
 /// Convenience functions for BanksClient
 #[async_trait]
@@ -32,7 +43,7 @@ pub trait BanksClientExtensions {
         &mut self,
         _ixs: &[Instruction],
         _payer: &Keypair,
-        _signers: Vec<&Keypair>,
+        _signers: Vec<&Keypair>
     ) -> Result<Transaction, BanksClientError> {
         unimplemented!();
     }
@@ -43,7 +54,7 @@ pub trait BanksClientExtensions {
     #[cfg(feature = "anchor")]
     fn get_anchor_account_data<T: AccountDeserialize>(
         &mut self,
-        _address: Pubkey,
+        _address: Pubkey
     ) -> Pin<Box<dyn Future<Output = Result<T, BanksClientError>> + '_>> {
         unimplemented!();
     }
@@ -55,7 +66,7 @@ pub trait BanksClientExtensions {
         _to: &Keypair,
         _lamports: u64,
         _space: u64,
-        _owner: Pubkey,
+        _owner: Pubkey
     ) -> transport::Result<()> {
         unimplemented!();
     }
@@ -67,7 +78,7 @@ pub trait BanksClientExtensions {
         _authority: &Pubkey,
         _freeze_authority: Option<&Pubkey>,
         _decimals: u8,
-        _payer: &Keypair,
+        _payer: &Keypair
     ) -> transport::Result<()> {
         unimplemented!();
     }
@@ -78,7 +89,7 @@ pub trait BanksClientExtensions {
         _account: &Keypair,
         _authority: &Pubkey,
         _mint: &Pubkey,
-        _payer: &Keypair,
+        _payer: &Keypair
     ) -> transport::Result<()> {
         unimplemented!();
     }
@@ -88,7 +99,7 @@ pub trait BanksClientExtensions {
         &mut self,
         _authority: &Pubkey,
         _mint: &Pubkey,
-        _payer: &Keypair,
+        _payer: &Keypair
     ) -> transport::Result<Pubkey> {
         unimplemented!();
     }
@@ -100,16 +111,18 @@ impl BanksClientExtensions for BanksClient {
         &mut self,
         ixs: &[Instruction],
         payer: &Keypair,
-        signers: Vec<&Keypair>,
+        signers: Vec<&Keypair>
     ) -> Result<Transaction, BanksClientError> {
         let latest_blockhash = self.get_latest_blockhash().await?;
 
-        Ok(Transaction::new_signed_with_payer(
-            ixs,
-            Some(&payer.pubkey()),
-            &signers,
-            latest_blockhash,
-        ))
+        Ok(
+            Transaction::new_signed_with_payer(
+                ixs,
+                Some(&payer.pubkey()),
+                &signers,
+                latest_blockhash
+            )
+        )
     }
 
     #[cfg(feature = "anchor")]
@@ -130,19 +143,20 @@ impl BanksClientExtensions for BanksClient {
         to: &Keypair,
         lamports: u64,
         space: u64,
-        owner: Pubkey,
+        owner: Pubkey
     ) -> transport::Result<()> {
         let latest_blockhash = self.get_latest_blockhash().await?;
 
-        self.process_transaction(system_transaction::create_account(
-            from,
-            to,
-            latest_blockhash,
-            lamports,
-            space,
-            &owner,
-        ))
-        .await
+        self.process_transaction(
+            system_transaction::create_account(
+                from,
+                to,
+                latest_blockhash,
+                lamports,
+                space,
+                &owner
+            )
+        ).await
         .map_err(Into::into)
     }
 
@@ -152,7 +166,7 @@ impl BanksClientExtensions for BanksClient {
         authority: &Pubkey,
         freeze_authority: Option<&Pubkey>,
         decimals: u8,
-        payer: &Keypair,
+        payer: &Keypair
     ) -> transport::Result<()> {
         let latest_blockhash = self.get_latest_blockhash().await?;
         self.process_transaction(system_transaction::create_account(
@@ -161,10 +175,8 @@ impl BanksClientExtensions for BanksClient {
             latest_blockhash,
             Rent::default().minimum_balance(spl_token::state::Mint::get_packed_len()),
             spl_token::state::Mint::get_packed_len() as u64,
-            &spl_token::id(),
-        ))
-        .await
-        .unwrap();
+            &spl_token::id()
+        )).await.unwrap();
 
         let ix = spl_token::instruction::initialize_mint(
             &spl_token::id(),
@@ -175,13 +187,14 @@ impl BanksClientExtensions for BanksClient {
         )
         .unwrap();
 
-        self.process_transaction(Transaction::new_signed_with_payer(
-            &[ix],
-            Some(&payer.pubkey()),
-            &[payer],
-            latest_blockhash,
-        ))
-        .await
+        self.process_transaction(
+            Transaction::new_signed_with_payer(
+                &[ix],
+                Some(&payer.pubkey()),
+                &[payer],
+                latest_blockhash
+            )
+        ).await
         .map_err(Into::into)
     }
 
@@ -190,7 +203,7 @@ impl BanksClientExtensions for BanksClient {
         account: &Keypair,
         authority: &Pubkey,
         mint: &Pubkey,
-        payer: &Keypair,
+        payer: &Keypair
     ) -> transport::Result<()> {
         let latest_blockhash = self.get_latest_blockhash().await?;
         self.process_transaction(system_transaction::create_account(
@@ -199,26 +212,25 @@ impl BanksClientExtensions for BanksClient {
             latest_blockhash,
             Rent::default().minimum_balance(spl_token::state::Account::get_packed_len()),
             spl_token::state::Account::get_packed_len() as u64,
-            &spl_token::id(),
-        ))
-        .await
-        .unwrap();
+            &spl_token::id()
+        )).await.unwrap();
 
         let ix = spl_token::instruction::initialize_account(
             &spl_token::id(),
             &account.pubkey(),
             mint,
-            authority,
+            authority
         )
         .unwrap();
 
-        self.process_transaction(Transaction::new_signed_with_payer(
-            &[ix],
-            Some(&payer.pubkey()),
-            &[payer],
-            latest_blockhash,
-        ))
-        .await
+        self.process_transaction(
+            Transaction::new_signed_with_payer(
+                &[ix],
+                Some(&payer.pubkey()),
+                &[payer],
+                latest_blockhash
+            )
+        ).await
         .map_err(Into::into)
     }
 
@@ -226,19 +238,24 @@ impl BanksClientExtensions for BanksClient {
         &mut self,
         account: &Pubkey,
         mint: &Pubkey,
-        payer: &Keypair,
-    ) -> transport::Result<Pubkey> {
+        payer: &Keypair
+    ) -> transport::Result<Pubkey>  {
         let latest_blockhash = self.get_latest_blockhash().await?;
         let associated_token_account = get_associated_token_address(account, mint);
-        let ix = create_associated_token_account(&payer.pubkey(), &account, &mint);
+        let ix = create_associated_token_account(
+            &payer.pubkey(), 
+            &account, 
+            &mint
+        );
 
-        self.process_transaction(Transaction::new_signed_with_payer(
-            &[ix],
-            Some(&payer.pubkey()),
-            &[payer],
-            latest_blockhash,
-        ))
-        .await?;
+        self.process_transaction(
+            Transaction::new_signed_with_payer(
+                &[ix],
+                Some(&payer.pubkey()),
+                &[payer],
+                latest_blockhash
+            )
+        ).await?;
 
         return Ok(associated_token_account);
     }
